262497bf048a13a5d2734741d5eba2c8d3fa5a0b
Ankit Kafle <notankitff@gmail.com>
2026-02-03 21:33:45 +0545

    Intermediate-II

--- Foundation/LinkedList4.cpp
+++ Foundation/LinkedList4.cpp
@@ -144,7 +144,7 @@ int main()
  
     do{
 	
-    printf("Enter choice:\n 1 for Creatiom of Nodes\n 2 for Deletion at begining\n  3 for Deletion at ending\n  4 for Deletion at given location\n 5 for Display\n 6 for exist");
+    printf("Enter choice:\n 1 for Creatiom of Nodes\n 2 for Deletion at begining\n  3 for Deletion at ending\n  4 for Deletion at given location\n 5 for Display\n 6 for eist");
     scanf("%d",&ch);
 	switch(ch){
 
